08ed8666492f411b435fc2bbf4f75313afe13fc3
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-10-06 14:29:24 +0200

    Added list vm ids func method.

--- modules/opennode/vm/__init__.py
+++ modules/opennode/vm/__init__.py
@@ -4,6 +4,9 @@ from func.minion.modules.opennode.common import delegate_methods
 
 from opennode.cli.actions import vm as mod
 
+from opennode.cli.actions.vm import openvz
+
+
 class VM(func_module.FuncModule):
     version = "0.0.1"
     api_version = "0.0.1"
@@ -11,5 +14,8 @@ class VM(func_module.FuncModule):
 
     def vm_types(self):
         return mod.vm_types.keys()
+
+    def list_vm_ids(self):
+        return.openvz._get_openvz_ct_id_list()
 
 delegate_methods(VM, mod)
