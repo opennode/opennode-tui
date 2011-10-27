b0d14016889289a0f47db38fb9b99e7fa1ad17f7
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-10-27 10:39:57 +0200

    Updated sync command for test machine.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo rsync -av --delete modules/opennode/ /usr/local/lib/python2.7/dist-packages/func/minion/modules/opennode/
-rsync -av --delete modules/opennode/ root@on6test:/usr/lib/python2.6/site-packages/func/minion/modules/opennode/
+#sudo rsync -av --delete modules/opennode/ /usr/local/lib/python2.7/dist-packages/func/minion/modules/opennode/
+sudo rsync -av --delete modules/onode/ root@on6test:/usr/lib/python2.6/site-packages/func/minion/modules/onode/
