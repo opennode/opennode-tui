69603b65b12e00ffa8a6769ae004c861c6d16f58
Ilja Livenson <ilja.livenson@gmail.com>
2011-11-05 23:30:12 +0100

    better configuration file location

--- opennode/cli/config.py
+++ opennode/cli/config.py
@@ -1,26 +1,40 @@
 import ConfigParser
+import os
 
-config_paths = {'global': 'opennode-tui.conf', #'/etc/opennode/opennode-tui.conf',
-           'openvz': 'openvz.conf', #'/etc/opennode/openvz.conf',
-           'kvm': '/etc/opennode/kvm.conf'}
+config_path = '/etc/opennode/'
 
-def c(group, field, config = 'global'):
+config_names = {'global': 'opennode-tui.conf', 
+           'openvz': 'openvz.conf',
+           'kvm': 'kvm.conf'}
+
+def _resolve_conf_name(conf_type):
+    """ Return a filename of the configuration file. Local file has a higher priority than in the config_path """
+    fnm = config_names[conf_type]
+    if os.path.isfile(fnm):
+        return fnm
+    elif os.path.isfile(config_path + fnm):
+        return config_path + fnm
+    else:
+        raise RuntimeError, "Missing configuration file for %s" %conf_type
+
+def c(group, field, conf_type = 'global'):
     """Get configuration value"""
     conf = ConfigParser.RawConfigParser()
-    conf.read(config_paths[config])
+    conf.read(_resolve_conf_name(conf_type))
     return conf.get(group, field)
 
-def cs(group, field, value, config = 'global'):
+def cs(group, field, value, conf_type = 'global'):
     """Set configuration value"""
     conf = ConfigParser.RawConfigParser()
-    conf.read(config_paths[config])
+    fnm = _resolve_conf_name(conf_type)
+    conf.read(fnm)
     conf.set(group, field, value)
-    with open(config_paths[config], 'wb') as configfile:
+    with open(fnm, 'wb') as configfile:
         conf.write(configfile)
 
-def clist(group, config = 'global'):
+def clist(group, conf_type = 'global'):
     """List configuration values"""
     conf = ConfigParser.RawConfigParser()
-    conf.read(config_paths[config])
+    conf.read(_resolve_conf_name(conf_type))
     return conf.items(group)
 
