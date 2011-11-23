a7a7054c1b8d003cdc7a85d55cb448295de6a5cb
Marko Mikulicic <marko.mikulicic@isti.cnr.it>
2011-11-23 18:21:30 +0100

    Modified sync to also sync tui module.

--- sync.sh
+++ sync.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 #sudo rsync -av --delete modules/opennode/ /usr/local/lib/python2.7/dist-packages/func/minion/modules/opennode/
-sudo rsync -av --delete modules/onode/ root@on6test:/usr/lib/python2.6/site-packages/func/minion/modules/onode/
+sudo chown $USER:$USER -R .
+rsync -av --delete modules/onode/ root@on6test:/usr/lib/python2.6/site-packages/func/minion/modules/onode/
+rsync -av --delete opennode/ root@on6test:/usr/lib/python2.6/site-packages/opennode/
