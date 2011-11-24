a1a170106157089f4715625321b89bd518f3afbf
Ilja Livenson <ilja.livenson@gmail.com>
2011-11-24 20:32:58 +0100

    display of purely local templates in the template management list

--- opennode/cli/screen.py
+++ opennode/cli/screen.py
@@ -234,8 +234,10 @@ class OpenNodeTUI(object):
     def display_select_template_from_repo(self, repo, storage_pool = c('general', 'default-storage-pool')): 
         remote_templates = actions.templates.get_template_list(repo)
         local_templates = actions.templates.get_local_templates(storage_pool, c(repo, 'type'))
-        list_items = [(tmpl, tmpl, tmpl in local_templates) for tmpl in remote_templates]
-        return display_checkbox_selection(self.screen, TITLE, list_items, 'Please, select templates for synchronisation')
+        list_items = [('(r)' + tmpl, tmpl, tmpl in local_templates) for tmpl in remote_templates]
+        purely_local_templates = list(set(local_templates) - set(remote_templates))
+        list_items.extend([('(l)' + tmpl, tmpl, tmpl in local_templates) for tmpl in purely_local_templates])
+        return display_checkbox_selection(self.screen, TITLE, list_items, 'Please, select templates to keep in the storage pool:')
 
     def display_select_template_from_storage(self, storage_pool, vm_type):
         """Displays a list of templates from a specified storage pool"""
