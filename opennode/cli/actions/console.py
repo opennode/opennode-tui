33838710dfcc60141cb2336a9df9da7119c3eee3
Ilja Livenson <ilja.livenson@gmail.com>
2011-12-06 09:12:37 +0100

    modified default openvz console to be a restricted bash shell with some simplistic PS1 env. First version.

--- opennode/cli/actions/console.py
+++ opennode/cli/actions/console.py
@@ -4,7 +4,9 @@ def run_kvm():
     __run_shell('qemu:///system')
 
 def run_openvz():
-    __run_shell('openvz:///system')
+    ps1_string = "[vzctl --help | vzlist | vzctl enter CT | exit] #"
+    os.system("PS1='%s ' bash --norc -i -r " % ps1_string)
+    #__run_shell('openvz:///system')
 
 def __run_shell(driver):
     os.system('virsh -c ' + driver)
