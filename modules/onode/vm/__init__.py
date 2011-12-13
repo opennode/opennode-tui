opennode/opennode-tui

commit 7b988230afab6e3eac2a76c65c51c2e801da0b6c

Fixed issue with parsing of /proc/net/dev

modules/onode/vm/__init__.py
@@ -345,7 +345,7 @@ class VM(func_module.FuncModule):
                 return float(execute("vzctl exec %s \"free | tail -n 2 | head -n 1 |awk '{print \$3 / 1024}'\"" % vm.ID()))
             def network_usage():
                 def get_netstats():
-                    return [int(v) for v in execute("vzctl exec %s \"cat /proc/net/dev|grep venet0 |awk '{print \$2, \$10}'\""
+                    return [int(v) for v in execute("vzctl exec %s \"cat /proc/net/dev|grep venet0 | awk -F: '{print $2}' | awk '{print \$1, \$9}'\""
                                                     % vm.ID()).split(' ')]
 
                 t2, (rx2, tx2) = time.time(), get_netstats()

modules/onode/__init__.py
@@ -28,7 +28,7 @@ class OpenNode(func_module.FuncModule):
             return float(execute("free | tail -n 2 | head -n 1 |awk '{print $3 / 1024}'"))
         def network_usage():
             def get_netstats():
-                return [int(v) for v in execute("grep eth0 /proc/net/dev |awk '{print $2, $10}'").split(' ')]
+                return [int(v) for v in execute("grep eth0 /proc/net/dev | awk -F: '{print $2}' | awk '{print $1, $9}'").split(' ')]
 
             t2, (rx2, tx2) = time.time(), get_netstats()
             t1, rx1, tx1 = roll_data("/tmp/func-network-host", (t2, rx2, tx2), (0, 0, 0))

commit cca41f16c6c3873b7cf1577fbf3230495d3bd2a3

Fixed issue with bash escapes, introduced in 7b98823

modules/onode/vm/__init__.py
@@ -345,7 +345,7 @@ class VM(func_module.FuncModule):
                 return float(execute("vzctl exec %s \"free | tail -n 2 | head -n 1 |awk '{print \$3 / 1024}'\"" % vm.ID()))
             def network_usage():
                 def get_netstats():
-                    return [int(v) for v in execute("vzctl exec %s \"cat /proc/net/dev|grep venet0 | awk -F: '{print $2}' | awk '{print \$1, \$9}'\""
+                    return [int(v) for v in execute("vzctl exec %s \"cat /proc/net/dev|grep venet0 | awk -F: '{print \$2}' | awk '{print \$1, \$9}'\""
                                                     % vm.ID()).split(' ')]
 
                 t2, (rx2, tx2) = time.time(), get_netstats()