5af11cc7741eb4642d0e0768f65c1f7929d95a56
Ilja Livenson <ilja.livenson@gmail.com>
2012-01-16 19:18:14 +0200

    removed unused configuration

--- modules/onode/vm/__init__.py
+++ modules/onode/vm/__init__.py
@@ -4,14 +4,11 @@ from certmaster.config import BaseConfig, ListOption
 from func.minion.modules import func_module
 from func.minion.modules.onode.common import delegate_methods
 
+
 class VM(func_module.FuncModule):
     version = "0.0.1"
     api_version = "0.0.1"
     description = "opennode vm module"
-
-    class Config(BaseConfig):
-        backends = ListOption()
-
 
 from opennode.cli.actions import vm as mod
 delegate_methods(VM, mod)
