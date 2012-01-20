d37b2db0f237a241c3ddabb4e215532d1db7c480
Ilja Livenson <ilja.livenson@gmail.com>
2012-01-20 20:28:02 +0200

    another improvement to a message string

--- opennode/cli/screen.py
+++ opennode/cli/screen.py
@@ -389,7 +389,7 @@ class OpenNodeTUI(object):
                 form = OpenvzModificationForm(self.screen, TITLE, available_vms[vm_id])
             else:
                 display_info(self.screen, TITLE,
-                    "At the moment only OpenVZ VMs\nediting is supported.")
+                    "Editing of '%s' VMs is not currently supported." % vm_type)
                 return self.display_vm_manage()
             # TODO KVM specific form
             user_settings = self._display_custom_form(form, available_vms[vm_id])
