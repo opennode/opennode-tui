b1d6efc770d11500f446ab2fa2540a8d3c1feebc
Andrei Sosnin <andrei.sosnin@gmail.com>
2012-11-13 18:13:17 +0200

    Moved implementation of host metrics to opennode.cli.actions (ON-584)

--- opennode/cli/actions/host.py
+++ opennode/cli/actions/host.py
@@ -1,6 +1,7 @@
 import netifaces
 import os
 import subprocess
+import time
 
 from opennode.cli.actions.utils import execute
 from opennode.cli import config
@@ -81,3 +82,46 @@ def disk_usage(partition=None):
                           'fstype':str(fstype),
                           'percentage':int(percentage[:-1])}
     return results
+
+
+def metrics():
+    from opennode.cli.actions.utils import execute, roll_data
+
+    def cpu_usage():
+        time_list_now = map(int, execute("head -n 1 /proc/stat").split(' ')[2:6])
+        time_list_was = roll_data('/tmp/func-cpu-host', time_list_now, [0] * 6)
+        deltas = [yi - xi for yi, xi in zip(time_list_now, time_list_was)]
+        try:
+            cpu_pct = 1 - (float(deltas[-1]) / sum(deltas))
+        except ZeroDivisionError:
+            cpu_pct = 0
+        return cpu_pct
+
+    def load():
+        return float(execute("cat /proc/loadavg | awk '{print $1}'"))
+
+    def memory_usage():
+        return float(execute("free | tail -n 2 | head -n 1 |awk '{print $3 / 1024}'"))
+
+    def network_usage():
+        def get_netstats():
+            iface = config.c('general', 'main_iface')
+            return [int(v) for v in \
+                    execute("grep %s: /proc/net/dev | awk -F: '{print $2}' | awk '{print $1, $9}'" % iface).split(' ')]
+        try:
+            t2, (rx2, tx2) = time.time(), get_netstats()
+            t1, rx1, tx1 = roll_data("/tmp/func-network-host", (t2, rx2, tx2), (0, 0, 0))
+
+            window = t2 - t1
+            return ((rx2 - rx1) / window, (tx2 - tx1) / window)
+        except ValueError:
+            return (0, 0)  # better this way
+
+    def diskspace_usage():
+        return float(execute("df -P |grep ' /$' | head -n 1 | awk '{print $3/1024}'"))
+
+    return dict(cpu_usage=cpu_usage(),
+                load=load(),
+                memory_usage=memory_usage(),
+                network_usage=max(network_usage()),
+                diskspace_usage=diskspace_usage())
