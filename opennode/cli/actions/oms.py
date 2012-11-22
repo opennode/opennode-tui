4417259e6a89f39feac4c7474737029a7783227b
Andrei Sosnin <andrei.sosnin@gmail.com>
2012-11-22 19:05:23 +0200

    Implemented minion registration through Salt (ON-721)

--- opennode/cli/actions/oms.py
+++ opennode/cli/actions/oms.py
@@ -1,4 +1,5 @@
-import ConfigParser
+import os
+import yaml
 
 from opennode.cli.actions.utils import execute
 from opennode.cli import config
@@ -6,39 +7,48 @@ from opennode.cli import config
 
 def get_oms_server():
     """Read OMS server port and address from the configuration file"""
-    minion_conf_file = config.c('general', 'minion-conf')
-    minion_config = ConfigParser.RawConfigParser()
-    minion_config.read(minion_conf_file)
-    try:
-        oms_server = minion_config.get('main', 'certmaster')
-        oms_server_port = minion_config.get('main', 'certmaster_port')
+    minion_conf_file = config.c('salt', 'minion-conf')
+    with open(minion_conf_file, 'r') as minion_conf:
+        minion_config = yaml.safe_load(minion_conf.read())
+        oms_server = minion_config.get('master', 'localhost')
+        oms_server_port = minion_config.get('master_port', 4506)
         return (oms_server, oms_server_port)
-    except ConfigParser.NoOptionError:
-        return ('', '')
 
 
-def set_oms_server(server, port=51235):
+def set_oms_server(server, port=4506):
     """Write OMS server address and port to the configuration file"""
     minion_conf_file = config.c('general', 'minion-conf')
-    minion_config = ConfigParser.RawConfigParser()
-    minion_config.read(minion_conf_file)
-    minion_config.set('main', 'certmaster', server)
-    minion_config.set('main', 'certmaster_port', port)
+
+    with open(minion_conf_file, 'r') as minion_conf:
+        minion_config = yaml.safe_load(minion_conf.read())
+        if minion_config.get('master', None):
+            minion_config['master'] = server
+        if minion_config.get('master_port', None):
+            minion_config['master_port'] = port
+
     with open(minion_conf_file, 'w') as conf:
-        minion_config.write(conf)
+        yaml.dump(minion_config, conf, default_flow_style=False)
 
 
 def register_oms_server(server, port):
     """Register with a new OMS server:port."""
     # cleanup of the previous func cert
-    execute('rm -fv /etc/pki/certmaster/`hostname -f`.*')
     set_oms_server(server, port)
-    execute('service funcd restart')
+    execute('service salt-minion restart')
 
 
 ## OMS VM specific ##
-def configure_oms_vm(ctid, hostname):
+def configure_oms_vm(ctid, ipaddr):
     """Adjust configuration of the VM hosting OMS"""
     base = "/vz/private/%s/" % ctid
     # set a hostname to be used as a binding interface
-    execute("sed -i 's/^listen_addr.*/listen_addr = %s/' %s/etc/certmaster/certmaster.conf" % (hostname, base))
+    master_conf_file = config.c('salt', 'master-conf')
+    master_conf_file = os.path.join([base, master_conf_file])
+
+    with open(master_conf_file, 'r') as master_conf:
+        master_config = yaml.safe_load(master_conf.read())
+        if master_config.get('master', None):
+            master_config['interface'] = ipaddr
+
+    with open(master_conf_file, 'w') as conf:
+        yaml.dump(master_config, conf, default_flow_style=False)
