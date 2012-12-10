73f5aba0530472e172691d1d14984349854bd07a
Ilja Livenson <ilja.livenson@gmail.com>
2012-12-10 19:21:43 +0200

    Updates to sync-simple.sh script

--- sync-simple.sh
+++ sync-simple.sh
@@ -1,2 +1,2 @@
-rsync -av --delete --exclude '*.pyc' modules/onode/ /usr/lib/python2.6/site-packages/func/minion/modules/onode/
 rsync -av --delete --exclude '*.pyc' opennode/ /usr/lib/python2.6/site-packages/opennode/
+rsync -av --delete --exclude '*.pyc' scripts/ /opt/opennode/bin
