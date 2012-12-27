b735d2e2a32d635daab637aebc014f8998944c11
Andres Rand <hrrand@gmail.com>
2012-12-27 16:34:56 +0200

    Reverted back to singelton pattern (ON-494)

--- opennode/cli/log.py
+++ opennode/cli/log.py
@@ -1,26 +1,30 @@
 import logging
+import logging.handlers
 import os
 import inspect
 from opennode.cli.config import get_config
 
-def get_logger(ext_log = None, level = 0):
-    if ext_log is None:
-        return Logger(level=level)
-    else:
-        return Logger(ext_log, level=level)
+_logger = None
 
+def get_logger():
+    global _logger
+    if _logger is None:
+        _logger = Logger()
+        return _logger
+    return _logger
 
 class Logger():
     """ Wrapper around standard logger, configured for our use case """
     logger = None
     def __init__(self, logger = None, level=0):
+        # For future: find a safe way to use external std logger facility
         if logger is None:
             self.logger = logging.getLogger('opennode-tui')
             self.logger.setLevel(logging.DEBUG)
             self.format_str = '%(asctime)s - %(levelname)7s - %(stack)14s - %(message)s'
             self.formatter = logging.Formatter(self.format_str)
-
-            self.fh = logging.FileHandler(get_config().getstring('general', 'log-location', '/var/log/opennode-tui.log'))
+            self.fh = logging.handlers.WatchedFileHandler(get_config().getstring('general', 'log-location',
+                                                                                 '/var/log/opennode-tui.log'))
             self.fh.setLevel(logging.DEBUG)
             self.fh.setFormatter(self.formatter)
             self.ch = logging.StreamHandler()
