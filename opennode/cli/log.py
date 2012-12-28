8607fa22a2eb26250e36b1e1b875528a60643f6c
Andres Rand <hrrand@gmail.com>
2012-12-28 15:18:12 +0200

    Added cli parameter and conf parameter for loglevel (ON-494)

--- opennode/cli/log.py
+++ opennode/cli/log.py
@@ -6,21 +6,32 @@ from opennode.cli.config import get_config
 
 _logger = None
 
-def get_logger():
+def get_logger(level=None):
     global _logger
     if _logger is None:
-        _logger = Logger()
+        _logger = Logger(level=level)
         return _logger
     return _logger
 
 class Logger():
     """ Wrapper around standard logger, configured for our use case """
     logger = None
-    def __init__(self, logger = None, level=0):
+    def __init__(self, logger = None, level=None):
         # For future: find a safe way to use external std logger facility
         if logger is None:
             self.logger = logging.getLogger('opennode-tui')
-            self.logger.setLevel(logging.DEBUG)
+            conf_level = get_config().getstring('general', 'loglevel', 'INFO')
+            if level is None:
+                try:
+                    level_nr = logging._levelNames[conf_level.upper()]
+                except KeyError:
+                    level_nr = logging.INFO
+            else:
+                try:
+                    level_nr = logging._levelNames[level.upper()]
+                except KeyError:
+                    level_nr = logging.INFO
+            self.logger.setLevel(level_nr)
             self.format_str = '%(asctime)s - %(levelname)7s - %(stack)14s - %(message)s'
             self.formatter = logging.Formatter(self.format_str)
             self.fh = logging.handlers.WatchedFileHandler(get_config().getstring('general', 'log-location',
@@ -34,8 +45,6 @@ class Logger():
             self.logger.addHandler(self.ch)
         else:
             self.logger = logger
-        if level:
-            self.logger.setLevel(level)
 
     # TODO: if need rises, add additional log levels.
     # inspect.stack() is needed to get filename where logging command came from.
