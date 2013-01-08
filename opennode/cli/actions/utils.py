7c6404c9fe952bfb9e933990dbd5f2c63f63785b
Andres Rand <hrrand@gmail.com>
2013-01-08 14:13:56 +0200

    Added proxy support for cURL http://user:pass@host/file downloads, code cleanup (TUI-24)

--- opennode/cli/actions/utils.py
+++ opennode/cli/actions/utils.py
@@ -171,17 +171,24 @@ class BasicURLOpener(urllib.FancyURLopener):
 
 def download(remote, local):
     """Download a remote file to a local file, using optional username/password
-    for basic HTTP authentication"""
-    """Using CURL as external dependency - progressbar, continuation of broken
-    downloads and user auth comes for free (provided we are not behind proxy)"""
+    for basic HTTP authentication. Using cURL as external dependency"""
     msg = "Getting remote file %s" % remote
     get_logger().info(msg)
     print msg
-    subprocess.call(['curl', '-C', '-', '-o', '%s' % local, '%s' % remote])
-    # url = urlparse.urlsplit(remote)
-    # opener = BasicURLOpener(url.username, url.password)
-    # download_monitor = ConsoleProgressBar(url.path.split('/')[-1])
-    # opener.retrieve(remote, local, download_monitor.download_hook)
+    url = urlparse.urlsplit(remote)
+    if url.username:
+        if 'http_proxy' in os.environ:
+            if url.password:
+                remote = remote.replace('%s:%s@' % (url.username, url.password), '')
+                userauth = '%s:%s' % (url.username, url.password)
+            else:
+                remote = remote.replace('%s@' % url.username, '')
+                userauth = url.username
+            subprocess.call(['curl', '--anyauth', 
+                             '--user', userauth,
+                             '-C', '-', '-o', '%s' % local, '%s' % remote])
+    else:
+        subprocess.call(['curl', '-C', '-', '-o', '%s' % local, '%s' % remote])
 
 
 def urlopen(remote):
