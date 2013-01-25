883b688185d989ac65c1f0d7d10bd6584b055cdf
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-01-25 13:53:40 +0200

    Removed func reference from sync.sh

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ fi
 set -e
 #sudo rsync -av --delete modules/opennode/ /usr/local/lib/python2.7/dist-packages/func/minion/modules/opennode/
 sudo chown $USER:$USER -R .
-rsync -av --delete --exclude '*.pyc' modules/onode/ root@${HOST}:/usr/lib/python2.6/site-packages/func/minion/modules/onode/
+#rsync -av --delete --exclude '*.pyc' modules/onode/ root@${HOST}:/usr/lib/python2.6/site-packages/func/minion/modules/onode/
 rsync -av --delete --exclude '*.pyc' opennode/cli/actions/ root@${HOST}:/usr/lib/python2.6/site-packages/salt/modules/onode/
 rsync -av --delete --exclude '*.pyc' opennode/ root@${HOST}:/usr/lib/python2.6/site-packages/opennode/
 rsync -av --delete --exclude '*.pyc' scripts/ root@${HOST}:opennode/
