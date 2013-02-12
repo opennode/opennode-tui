e417bafbe374ba50b00abf1b0635e81776e40160
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-02-12 21:31:22 +0200

    Revert "Attempt at limiting possibility of stray subprocesses on agent side (OMS-207)"
    This reverts commit 42cd2c70c0d479d2e72a1db968279dceec9ccd4d.

--- opennode/cli/actions/utils.py
+++ opennode/cli/actions/utils.py
@@ -76,16 +76,13 @@ def execute(cmd):
 def execute2(cmd):
     get_logger().debug('execute2 cmd: %s', cmd)
     args = shlex.split(cmd)
-    try:
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        while p.poll() is None:
-            line = p.stdout.readline()
-            yield line
-    except Exception:
-        p.kill()
-        raise
-    finally:
-        del p
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while(True):
+        retcode = p.poll()  # returns None while subprocess is running
+        line = p.stdout.readline()
+        yield line
+        if(retcode is not None):
+            break
 
 
 def calculate_hash(target_file):
