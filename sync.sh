a8b7bed104b80e973d95ca0af291faadd8c1b2b8
Ilja Livenson <ilja.livenson@gmail.com>
2013-03-19 11:44:54 +0200

    Reverted sync.sh used for development (TUI-57)

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOST="$1"
+
+if [ ! $HOST ]; then
+        HOST=on6test
+fi
+
+set -e
+sudo chown $USER:$USER -R .
+rsync -av --delete --exclude '*.pyc' opennode/cli/actions/ root@${HOST}:/usr/lib/python2.6/site-packages/salt/modules/onode/
+rsync -av --delete --exclude '*.pyc' opennode/ root@${HOST}:/usr/lib/python2.6/site-packages/opennode/
+rsync -av --delete --exclude '*.pyc' scripts/ root@${HOST}:opennode/
+
+ssh root@${HOST} service salt-minion restart
+
