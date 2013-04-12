6a3f29db2da7ceca2c4fcf0484bd903564b96382
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-04-12 13:09:33 +0300

    Refactor: eliminated Logger class in favor of standard logging (TUI-64)

--- opennode/cli/log.py
+++ opennode/cli/log.py
@@ -1,69 +1,36 @@
 import logging
 import logging.handlers
-import os
-import inspect
+import sys
+
 from opennode.cli.config import get_config
 
 _logger = None
 
 def get_logger(level=None):
-    global _logger
-    if _logger is None:
-        _logger = Logger(level=level)
-        return _logger
-    return _logger
+    logger = logging.getLogger('opennode-tui')
+    if not getattr(logger, '_configured', False):
 
-class Logger():
-    """ Wrapper around standard logger, configured for our use case """
-    logger = None
-    def __init__(self, logger = None, level=None):
-        # For future: find a safe way to use external std logger facility
-        if logger is None:
-            self.logger = logging.getLogger('opennode-tui')
+        if level is None:
             conf_level = get_config().getstring('general', 'loglevel', 'INFO')
+            level = logging._levelNames.get(conf_level.upper())
             if level is None:
-                try:
-                    level_nr = logging._levelNames[conf_level.upper()]
-                except KeyError:
-                    level_nr = logging.INFO
-            else:
-                try:
-                    level_nr = logging._levelNames[level.upper()]
-                except KeyError:
-                    level_nr = logging.INFO
-            self.logger.setLevel(level_nr)
-            self.format_str = '%(asctime)s - %(levelname)7s - %(stack)14s - %(message)s'
-            self.formatter = logging.Formatter(self.format_str)
-            self.fh = logging.handlers.WatchedFileHandler(get_config().getstring('general', 'log-location',
-                                                                                 '/var/log/opennode-tui.log'))
-            self.fh.setLevel(logging.DEBUG)
-            self.fh.setFormatter(self.formatter)
-            self.ch = logging.StreamHandler()
-            self.ch.setLevel(logging.ERROR)
-            self.ch.setFormatter(self.formatter)
-            self.logger.addHandler(self.fh)
-            self.logger.addHandler(self.ch)
-        else:
-            self.logger = logger
+                level = logging.INFO
 
-    # TODO: if need rises, add additional log levels.
-    # inspect.stack() is needed to get filename where logging command came from.
-    def debug(self, text, *args):
-        stack = {'stack': os.path.basename(inspect.stack()[1][1])}
-        self.logger.debug(text, *args, extra=stack)
+        logger.setLevel(level)
 
-    def info(self, text, *args):
-        stack = {'stack': os.path.basename(inspect.stack()[1][1])}
-        self.logger.info(text, *args, extra=stack)
+        fh = logging.handlers.WatchedFileHandler(get_config().getstring('general', 'log-location',
+                                                                        '/var/log/opennode-tui.log'))
+        format_str = '%(asctime)s %(levelname)7s %(module)10s:%(lineno)s:%(funcName)s - %(message)s'
+        fhformatter = logging.Formatter(format_str)
+        fh.setFormatter(fhformatter)
+        logger.addHandler(fh)
 
-    def warn(self, text, *args):
-        stack = {'stack': os.path.basename(inspect.stack()[1][1])}
-        self.logger.warning(text, *args, extra=stack)
+        sh = logging.StreamHandler(sys.stdout)
+        sh.setLevel(logging.INFO)
+        sh.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(sh)
 
-    def error(self, text, *args):
-        stack = {'stack': os.path.basename(inspect.stack()[1][1])}
-        self.logger.error(text, *args, extra=stack)
-
-    def critical(self, text, *args):
-        stack = {'stack': os.path.basename(inspect.stack()[1][1])}
-        self.logger.critical(text, *args, extra=stack)
+        sherr = logging.StreamHandler()
+        sherr.setLevel(logging.ERROR)
+        sherr.setFormatter(logging.Formatter('%(module)10s:%(lineno)s:%(funcName)s - %(message)s'))
+        logger.addHandler(sherr)
