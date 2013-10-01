f932d7f939897b811b51f0042df287c89df1b9fa
Ilja Livenson <ilja.livenson@gmail.com>
2013-10-01 20:45:24 +0300

    Added explicit QEMU connection URI for disk detection script (LEV-151)

--- scripts/libvirt_detect_domain_devices.sh
+++ scripts/libvirt_detect_domain_devices.sh
@@ -3,5 +3,5 @@
 # libvirt domain
 domain=$1
 # output is expected to be a list of file-based devices
-virsh domblklist --details $domain |  grep ^file | awk '{print $4}'
+virsh -c qemu:///system domblklist --details $domain |  grep ^file | awk '{print $4}'
 
--- scripts/libvirt_get_device_size.sh
+++ scripts/libvirt_get_device_size.sh
@@ -3,5 +3,5 @@
 domain=$1
 device=$2
 
-virsh domblkinfo $domain $device |grep ^Capacity| awk '{print $2}'
+virsh -c qemu:///system domblkinfo $domain $device |grep ^Capacity| awk '{print $2}'
 
